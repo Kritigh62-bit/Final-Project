72c070817ec384e1bc97dc8629918082d9cd1e7f
Kritigh62-bit <kritigh62@gmail.com>
2026-08-18 18:08:07 +0545

    code

---
+++ Quiz.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+
+// QUESTION CLASS
+// Abstract Base Class
+class Question {
+protected:
+    string questionText;
+    int correctAnswer;
+    string category;
+
+public:
+
+    Question(string q, int answer, string cat) {
+        questionText = q;
+        correctAnswer = answer;
+        category = cat;
+    }
+
+    // Pure virtual function - Abstraction
+    virtual void display() = 0;
+
+    // Check answer
+    bool checkAnswer(int answer) {
+        return answer == correctAnswer;
+    }
+
+    // Get category
+    string getCategory() {
+        return category;
+    }
+
+    virtual ~Question() {}
+};
